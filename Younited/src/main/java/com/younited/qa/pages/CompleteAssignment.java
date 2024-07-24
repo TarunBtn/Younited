e10a9b69d37287ff5cfc4c4c9f6087b1b2a630b7
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-07-24 16:22:12 +0530

    CompleteAssignment

--- Younited/src/main/java/com/younited/qa/pages/CompleteAssignment.java
+++ Younited/src/main/java/com/younited/qa/pages/CompleteAssignment.java
@@ -1,5 +1,6 @@
 package com.younited.qa.pages;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
@@ -10,152 +11,264 @@ import com.younited.qa.base.TestBase;
 
 public class CompleteAssignment extends TestBase{
 	//Page factory or Object Repository
-	@FindBy(id="selected_company")
-	WebElement companyDropDown;
-	
-	@FindBy(xpath="//*[@id=\"selected_company\"]/option[2]")
-	WebElement selectCompany;
-	
-	@FindBy(xpath="//*[@id=\"general-company-edit\"]/div/form/div[10]/div[2]/div/div[1]")
-	WebElement sectorDropDown;
-	
-	@FindBy(xpath="//*[@id=\"sector-12\"]/span/span")
-	WebElement sectorSelect;
-	
-	@FindBy(id="werklocatie")
-	WebElement workLocation;
-	
-	@FindBy(xpath="//*[@id=\"general-company-edit\"]/div/form/div[12]/div[2]/div[2]")
-	WebElement workLocationTooltip;
-	
-	@FindBy(xpath="//*[@id=\"general-company-edit\"]/div/form/div[6]/div[2]/div[1]/div/button[2]")
-	WebElement generateDescription;
-	
-	@FindBy(xpath="//*[@id=\"chatGPT-module___BV_modal_body_\"]/div[2]/div[4]/div/div[2]/div")
-	WebElement useThisText;
-	
-	@FindBy(id="startdatumopdracht")
-	WebElement clickStartDateDropDown;
-	
-	@FindBy(xpath="//*[@id=\"assignment\"]/div[5]/div/div/div/div[1]/button[3]/i")
-	WebElement selectStartYear;
-	
-	@FindBy(xpath="//*[@id=\"assignment\"]/div[5]/div/div/div/div[2]/table/tbody/tr[1]/td[7]/div")
-	WebElement selectStartDate;
-	
-	@FindBy(id="einddatumopdracht")
-	WebElement endDateDropDown;
-	
-	@FindBy(xpath="//*[@id=\"assignment\"]/div[5]/div/div/div/div[1]/button[3]/i")
-	WebElement selectEndYear;
-	
-	@FindBy(xpath="//*[@id=\"assignment\"]/div[5]/div/div/div/div[2]/table/tbody/tr[2]/td[1]/div")
-	WebElement selectEndDate;
-	
-	@FindBy(xpath="//*[@id=\"aantaluurperweek\"]/div/div[2]/div[2]/div[1]")
-	WebElement numberOfHours;
-	
-	@FindBy(xpath="//*[@id=\"uurtarief\"]/div/div[4]/div[1]")
-	WebElement hourlyRateFromTo;
-	
-	@FindBy(name="opleidingsniveau")
-	WebElement workingThinkingLevel;
-	
-	@FindBy(xpath="//*[@id=\"general-company-edit\"]/div/form/div[24]/div/button[2]")
-	WebElement clickSaveButton;
-	
-	@FindBy(xpath="//*[@id=\"general-company-edit\"]/div/form/div[20]/div[2]/div/div/div[1]")
-	WebElement clickFieldOfExpertise;
-		
-	@FindBy(xpath="//*[@id=\"vakgebied-6\"]/span/span")
-	WebElement selectFieldOfExpertise;
-	
-	//Initialization
-	public CompleteAssignment(){
-		PageFactory.initElements(Driver, this);
-	}
-	
-	//Actions
-	public void selectCompany() {
-		companyDropDown.click();
-		selectCompany.click();
-	}
-	
-	public void clickSectorDropDown() {
-		sectorDropDown.click();
-	}
-	
-	public void selectSectorFromDropDown() {
-		sectorSelect.click();
-	}
-	
-	public void selectWorkLocation() {
-		workLocation.sendKeys("Punthorst, Netherlands");		
-	}
-	
-	public void clickWorkLocationTooltip() {
-		//workLocationTooltip.click();
-	    Actions builder=new Actions(Driver);
-	    builder.moveToElement(workLocationTooltip).build().perform();
-		
-	}
-	
-	public void clickGenerateDescription() {
-		generateDescription.click();
-	}
-	
-	public void clickUseThisText() {
-		useThisText.click();
-	}
-	
-	public void clickStartDateDropDown() {
-		clickStartDateDropDown.click();			
-	}
-	
-	public void selectStartYear() {
-		selectStartYear.click();
-	}
-	
-	public void selectStartDate() {
-		selectStartDate.click();
-	} 
-	
-	public void clickEndDateDropDown() {
-		endDateDropDown.click();
-	}
-	
-	public void selectEndYear() {
-		selectEndYear.click();
-	}
-	
-	public void selectEndDate() {
-		selectEndDate.click();
-	}
-	
-	public void selectNumberOfHours() {
-		numberOfHours.click();
-	}
-	
-	public void selectHourlyRateFromTo() {
-		hourlyRateFromTo.click();
-	}
-	
-	public void selectWorkingThinkingLevel() {
-		Select oSelect=new Select(workingThinkingLevel);
-		oSelect.selectByIndex(4);
-	}
-	
-	public void clickSaveButton() {
-		clickSaveButton.click();
-	}
-	
-	public void clickFieldOfExpertise() {
-		clickFieldOfExpertise.click();
-	}
-	
-	public void selectFieldOfExpertise() {
-		selectFieldOfExpertise.click();
-		
-	}
-
+			@FindBy(xpath="//*[@id=\"assignment-add\"]/span[2]/div/span/div[2]/form/div[1]/div[2]/div/div/div[1]")
+			WebElement companyDropDown;
+			
+			@FindBy(xpath="//*[@id=\"opdracht.selected_company-3\"]/span/span")
+			WebElement selectCompany;
+			
+			@FindBy(xpath="//input[@id='opdracht.naamopdracht']")
+			WebElement assignmentName;
+			
+			@FindBy(xpath="//*[@id=\"general-company-edit\"]/div/form/div[10]/div[2]/div/div[1]")
+			WebElement sectorDropDown;
+			
+			@FindBy(xpath="//*[@id=\"sector-1\"]/span/span")
+			WebElement sectorSelect;
+			
+			@FindBy(xpath="//div[@data-vv-as='Vakgebied']//div[@class='multiselect__select']")
+			WebElement disciplineDropDown;
+			
+			@FindBy(xpath="//span[contains(text(),'Engineering')]")
+			WebElement selectDescipline;
+			
+			@FindBy(xpath="//input[@id='opdracht.plaatsvanopdracht']")
+			WebElement workLocation;
+			
+			@FindBy(xpath="//*[@id=\"assignment-add\"]/span[2]/div/span/div[2]/form")
+			WebElement workLocationTooltip;
+			
+			@FindBy(xpath="//div[@aria-owns='listbox-opdracht.opleidingsniveau']//div[@class='multiselect__select']")
+			WebElement workingThinkingLevel;
+			
+			@FindBy(xpath="//*[@id=\"opdracht.opleidingsniveau-3\"]/span")
+			WebElement selectWorkingThinkingLevel;
+			
+			@FindBy(xpath="//input[@id='suggestionsForJobTitle']")
+			WebElement profile;
+			
+			@FindBy(xpath="//span[@data-text='Database administrator (m/v/x)']//strong//font//font[contains(text(),'Database')]")
+			WebElement profileTooltip;
+			
+			@FindBy(xpath="//*[@id=\"general-company-edit\"]/div/form/div[6]/div[2]/div[1]/div/button[2]")
+			WebElement generateDescription;
+			
+			@FindBy(xpath="//*[@id=\"chatGPT-module___BV_modal_body_\"]/div[2]/div[3]/div/div[2]/div")
+			WebElement useThisText;
+			
+			@FindBy(xpath="/html/body/div[8]/div[1]/div/div/div/div[2]/div[3]/div/div[2]/div")
+			WebElement useThisTextOne;
+			
+			@FindBy(xpath="//div[@id='opdracht.startdatumopdracht']//div//div[@class='dp__input_wrap']//div//*[name()='svg']")
+			WebElement clickStartDateDropDown;
+			
+			@FindBy(xpath="//*[@id=\"assignment\"]/div[7]/div/div/div/div[1]/button[4]/i")
+			WebElement selectStartYear;
+			
+			@FindBy(xpath="/html/body/div[1]/div/div[1]/span[2]/div/span/form/div[1]/div[2]/div/div[1]/div/div[2]/div/div[2]/div/div/div/div[2]/div/div[3]/div[5]/div[3]/div")
+			WebElement selectStartDate;
+			
+			@FindBy(xpath="/html/body/div[1]/div/div[1]/span[2]/div/span/form/div[2]/div[2]/div/div[1]/div/div/div/input")
+			WebElement endDateDropDown;
+			
+			@FindBy(xpath="//*[@id=\"assignment\"]/div[7]/div/div/div/div[1]/button[3]/i")
+			WebElement selectEndYear;
+			
+			@FindBy(xpath="/html/body/div[1]/div/div[1]/span[2]/div/span/form/div[2]/div[2]/div/div[1]/div/div[2]/div/div[2]/div/div/div/div[2]/div/div[3]/div[5]/div[4]/div")
+			WebElement selectEndDate;
+			
+			@FindBy(xpath="//*[@id=\"aantaluurperweek\"]/div/div[2]/div[2]/div[1]")
+			WebElement numberOfHours;
+			
+			@FindBy(xpath="//*[@id=\"uurtarief\"]/div/div[4]/div[1]")
+			WebElement hourlyRateFromTo;
+			
+			@FindBy(xpath="//*[@id=\"general-company-edit\"]/div/form/div[24]/div/button[2]")
+			WebElement clickSaveButton;
+			
+			@FindBy(xpath="//button[@class='btn btn-secondary btn-sm btn btn-primary next-btn']")
+			WebElement nextBtn;
+			
+			@FindBy(xpath="//*[@id=\"general-company-edit\"]/div/form/div[20]/div[2]/div/div/div[1]")
+			WebElement clickFieldOfExpertise;
+				
+			@FindBy(xpath="//*[@id=\"vakgebied-6\"]/span/span")
+			WebElement selectFieldOfExpertise;
+			
+			@FindBy(xpath="/html/body/div[1]/main/div/div[2]/div/form/span[3]/div[2]/div/div[2]/div[1]/input")
+			WebElement skillsField;
+			
+			@FindBy(xpath="/html/body/div[1]/main/div/div[2]/div/form/span[3]/div[2]/div/div[2]/div[2]/a[1]/div[2]/span/span")
+			WebElement skillsFieldToolTip;
+			
+			@FindBy(xpath="//*[@id=\"general-company-edit\"]/div/form/span[3]/div[2]/div/form/div/div/div[2]/div[1]/label")
+			WebElement unSelectSkillsOne;
+			
+			@FindBy(xpath="//*[@id=\"general-company-edit\"]/div/form/span[3]/div[2]/div/form/div/div/div[2]/div[2]/label")
+			WebElement unSelectSkillsTwo;
+			
+			@FindBy(xpath="/html/body/div[1]/div/div[1]/span[2]/div/span/div[2]/form/div[4]/div[2]/div/span[2]/div/div/div/input")
+			WebElement clickVisibleDateTo;
+			
+			@FindBy(xpath="/html/body/div[1]/div/div[1]/span[2]/div/span/div[2]/form/div[4]/div[2]/div/span[2]/div/div[2]/div/div[2]/div/div/div/div[2]/div/div[3]/div[5]/div[4]/div")
+			WebElement selectVisibleDateTo;
+			
+			//Initialization
+			public CompleteAssignment(){
+				PageFactory.initElements(Driver, this);
+			}
+			
+			//Actions
+			public void clickCompanyDropDown() {
+				companyDropDown.click();		
+			}
+			
+			public void selectCompany() {
+				selectCompany.click();
+			}
+			
+			public void assignmentName(String value) {
+				assignmentName.sendKeys(value);		
+			}
+			
+			public void clickSectorDropDown() {
+				sectorDropDown.click();
+			}
+			
+			public void selectSectorFromDropDown() {
+				sectorSelect.click();
+			}
+			
+			public void clickDesciplineDropDown() {
+				disciplineDropDown.click();
+			}
+			
+			public void selectDescipline() {
+				selectDescipline.click();
+			}
+			
+			public void clickWorkLocation() {
+				workLocation.click();
+			}
+			
+			public void selectWorkLocation(String value) {
+				workLocation.sendKeys(value);		
+			}
+			
+			public void clickWorkLocationTooltip() {
+				//workLocationTooltip.click();
+			    //Actions builder=new Actions(Driver);
+			    //builder.moveToElement(workLocationTooltip).build().perform();
+				workLocation.sendKeys(Keys.TAB);
+				
+			}
+			
+			public void clickProfileField() {
+				profile.click();
+			}
+			
+			public void enterProfile(String value) {
+				profile.sendKeys(value);
+			}
+			
+			public void clickProfileTooltip() {
+				//profileTooltip.click();
+				profile.sendKeys(Keys.TAB);
+			}
+			
+			public void clickGenerateDescription() {
+				generateDescription.click();
+			}
+			
+			public void clickUseThisText() {
+				useThisText.click();
+			}
+			
+			public void clickUseThisTextOne() {
+				useThisTextOne.click();
+			}
+			
+			public void clickStartDateDropDown() {
+				clickStartDateDropDown.click();			
+			}
+			
+			public void selectStartYear() {
+				selectStartYear.click();
+			}
+			
+			public void selectStartDate() {
+				selectStartDate.click();
+			} 
+			
+			public void clickEndDateDropDown() {
+				endDateDropDown.click();
+			}
+			
+			public void selectEndYear() {
+				selectEndYear.click();
+			}
+			
+			public void selectEndDate() {
+				selectEndDate.click();
+			}
+			
+			public void selectNumberOfHours() {
+				numberOfHours.click();
+			}
+			
+			public void selectHourlyRateFromTo() {
+				hourlyRateFromTo.click();
+			}
+			
+			public void clickWorkingThinkingLevel() {
+				workingThinkingLevel.click();
+			}
+			
+			public void selectWorkingThinkingLevel() {
+				selectWorkingThinkingLevel.click();
+			}
+			
+			public void clickSaveButton() {
+				clickSaveButton.click();
+			}
+			
+			public void clickNextButton() {
+				nextBtn.click();
+			}
+			
+			public void clickFieldOfExpertise() {
+				clickFieldOfExpertise.click();
+			}
+			
+			public void selectFieldOfExpertise() {
+				selectFieldOfExpertise.click();	
+			}
+			
+			public void clickSkillsField() {
+				skillsField.click();
+			}
+			
+			public void enterSkills(String value) {
+				skillsField.sendKeys(value);
+			}
+			
+			public void skillsToolTip() {
+				skillsFieldToolTip.click();
+			}
+			
+			public void unSelectSkillsOne() {
+				unSelectSkillsOne.click();
+			}
+			
+			public void unSelectSkillsTwo() {
+				unSelectSkillsTwo.click();
+			}
+			
+			public void clickVisibleDateTo() {
+				clickVisibleDateTo.click();
+			}
+			
+			public void selectVisibleDateTo() {
+				selectVisibleDateTo.click();
+			}
 }
--- Younited/src/test/java/com/younited/qa/testcases/CompleteAssignmentTest.java
+++ Younited/src/test/java/com/younited/qa/testcases/CompleteAssignmentTest.java
@@ -38,34 +38,76 @@ public class CompleteAssignmentTest extends TestBase{
 		completeAssignment=new CompleteAssignment();
 		testUtil=new TestUtil();		
 		homePage=loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
-		testUtil.testWaitEight();
+		testUtil.testWaitFourteen();
 		//homePage.clickAcceptAllCookies();
-		//testUtil.testWaitEight();		
+		//testUtil.testWaitEleven();		
 		homePage.clickMyAssignmentsLink();
-		testUtil.testWaitEleven();
-		testUtil.testWaitEleven();
-		addCommandPage=myAssignmentsPage.clickAddNewCommand();
-		testUtil.testWaitEight();												
+		testUtil.testWaitFourteen();
+		//testUtil.testWaitEight();
+		//addCommandPage=myAssignmentsPage.clickAddNewCommand();
+		myAssignmentsPage.clickAddNewCommand();
+		testUtil.testWaitEight();
+		testUtil.scrollDown();
+		testUtil.testWaitFour();
 		addCommandPage.clickChooseFile();		
 		testUtil.testWaitFour();
-		testUtil.uploadFile("D:\\Testfile.docx");
+		testUtil.uploadFile("D:\\Assignment.docx");
 		testUtil.testWaitEight();
 		addCommandPage.clickUploadFile();
 		testUtil.testWaitEight();
-		checkCommandPage.clickFillInComplete();
-		testUtil.testWaitEight();
+		testUtil.scrollUp();
+		testUtil.testWaitTwo();
+		//checkCommandPage.clickFillInComplete();
+		//testUtil.testWaitEight();
+		//testUtil.scrollDown();
+		//testUtil.testWaitFour();
 	}
 	
 	@Test
 	public void completeAssignmentTest()throws Exception {
+		completeAssignment.clickCompanyDropDown();
+		testUtil.testWaitTwo();
 		completeAssignment.selectCompany();
 		testUtil.testWaitFour();
-		completeAssignment.clickSectorDropDown();
+		testUtil.scrollDown();
 		testUtil.testWaitTwo();
-		completeAssignment.selectSectorFromDropDown();
-		testUtil.testWaitTwo();
-		completeAssignment.selectWorkLocation();
+		completeAssignment.assignmentName("QA EngineerEight");
 		testUtil.testWaitFour();
+		//completeAssignment.clickSectorDropDown();
+		//testUtil.testWaitTwo();
+		//completeAssignment.selectSectorFromDropDown();
+		//testUtil.testWaitFour();
+		testUtil.scrollDown();
+		testUtil.testWaitFour();
+		completeAssignment.clickDesciplineDropDown();
+		testUtil.testWaitTwo();
+		completeAssignment.selectDescipline();
+		testUtil.testWaitFour();
+		testUtil.scrollDown();
+		testUtil.testWaitFour();
+		completeAssignment.clickWorkLocation();
+		testUtil.testWaitTwo();
+		completeAssignment.selectWorkLocation("Punthorst, Netherlands");
+		testUtil.testWaitFour();
+		completeAssignment.clickWorkLocationTooltip();
+		testUtil.testWaitFour();
+		completeAssignment.clickWorkLocation();
+		testUtil.testWaitTwo();
+		completeAssignment.clickWorkLocationTooltip();
+		testUtil.testWaitFour();
+		completeAssignment.clickWorkingThinkingLevel();
+		testUtil.testWaitFour();
+		completeAssignment.selectWorkingThinkingLevel();
+		testUtil.testWaitFour();
+		completeAssignment.clickNextButton();
+		testUtil.testWaitEight();
+		//completeAssignment.clickNextButton();
+		//testUtil.testWaitFourteen();
+		//testUtil.scrollUp();
+		//testUtil.testWaitFour();
+		
+		//completeAssignment.selectWorkLocation();
+		//testUtil.testWaitFour();
 		//completeAssignment.clickWorkLocationTooltip();
 		//testUtil.testWaitFour();
 		//try {
@@ -74,51 +116,68 @@ public class CompleteAssignmentTest extends TestBase{
 		//}catch(ElementNotInteractableException e) {
 			//e.printStackTrace();
 		//}	
+		completeAssignment.clickProfileField();
 		testUtil.testWaitTwo();
+		completeAssignment.enterProfile("Database administrator");
+		testUtil.testWaitFour();
+		completeAssignment.clickProfileTooltip();
+		testUtil.testWaitFour();
+		completeAssignment.clickNextButton();
+		testUtil.testWaitEight();
 		testUtil.scrollUp();
-		testUtil.testWaitTwo();
-		testUtil.scrollUp();
-	    testUtil.testWaitEleven();
+		testUtil.testWaitFour();
+		/*testUtil.scrollUp();
+	    testUtil.testWaitFour();
 		//Generate description
 	    try {
 		    completeAssignment.clickGenerateDescription();
-		    testUtil.testWaitEight();
-		    //testUtil.testWaitFourteen();
-	    }catch(NoSuchElementException e) {
+		    //testUtil.testWaitEleven();
+	   }catch(NoSuchElementException e) {
+	   	  e.printStackTrace();
+	   }
+	    testUtil.scrollDown();
+	    testUtil.testWaitFour();
+	    //testUtil.scrollDown();
+	    //testUtil.testWaitTwo();
+	    try {
+		   completeAssignment.clickUseThisText();
+	    }catch(ElementClickInterceptedException e) {
 	    	e.printStackTrace();
-	    }	    
-		//completeAssignment.clickUseThisText();
-		//testUtil.testWaitEight();	   
-	    testUtil.testWaitTwo();;
-		testUtil.scrollDown();
-		testUtil.testWaitTwo();
-		testUtil.scrollDown();
-		testUtil.testWaitTwo();        
+	    }
+	    
+	    try {
+			   completeAssignment.clickUseThisTextOne();
+		    }catch(ElementClickInterceptedException e) {
+		    	e.printStackTrace();
+		    }
+		testUtil.testWaitEight();*/
 		//Select start date
-		completeAssignment.clickStartDateDropDown();
-		testUtil.testWaitTwo();
-		completeAssignment.selectStartYear();
-		testUtil.testWaitTwo();
+	    completeAssignment.clickStartDateDropDown();
+		testUtil.testWaitFour();
+		//completeAssignment.selectStartYear();
+		//testUtil.testWaitTwo();
 		completeAssignment.selectStartDate();
-		testUtil.testWaitTwo();		
+		testUtil.testWaitFour();	
 		//Select end date
 		completeAssignment.clickEndDateDropDown();
-		testUtil.testWaitTwo();
-		completeAssignment.selectEndYear();
-		testUtil.testWaitTwo();
-		completeAssignment.selectEndYear();
-		testUtil.testWaitTwo();
+		testUtil.testWaitFour();
+		//completeAssignment.selectEndYear();
+		//testUtil.testWaitTwo();
+		//completeAssignment.selectEndYear();
+		//testUtil.testWaitTwo();
 		completeAssignment.selectEndDate();
-		testUtil.testWaitTwo();
+		testUtil.testWaitFour();
 		testUtil.scrollDown();
 		testUtil.testWaitFour();
 		//NumberofHours
-		completeAssignment.selectNumberOfHours();
+		/*completeAssignment.selectNumberOfHours();
 		testUtil.testWaitFour();
+		testUtil.scrollDown();
+		testUtil.testWaitTwo();
 		//selectWorkingThinkingLevel
 		completeAssignment.selectWorkingThinkingLevel();
 		testUtil.testWaitFour();
-		//Select FieldOfExpertise
+		//Select FieldOfExpertise/Descipline
 		completeAssignment.clickFieldOfExpertise();
 		testUtil.testWaitFour();
 		completeAssignment.selectFieldOfExpertise();
@@ -129,13 +188,53 @@ public class CompleteAssignmentTest extends TestBase{
 		testUtil.testWaitTwo();
 		testUtil.scrollDown();
 		testUtil.testWaitTwo();
+		//Add Skills and remove skills
+		completeAssignment.clickSkillsField();
+		testUtil.testWaitTwo();
+		completeAssignment.enterSkills("Security Management");
+		testUtil.testWaitFour();
+		completeAssignment.skillsToolTip();
+		testUtil.testWaitTwo();
+		completeAssignment.enterSkills("Databases");
+		testUtil.testWaitFour();
+		completeAssignment.skillsToolTip();
+		testUtil.testWaitTwo();
+		completeAssignment.enterSkills("Test Engineering");
+		testUtil.testWaitFour();
+		completeAssignment.skillsToolTip();
+		testUtil.testWaitTwo();
+		completeAssignment.unSelectSkillsOne();
+		testUtil.testWaitTwo();
+		completeAssignment.unSelectSkillsTwo();
+		testUtil.testWaitTwo();*/
+		completeAssignment.clickNextButton();
+		testUtil.testWaitEight();
+		testUtil.scrollDown();
+		testUtil.testWaitFour();
+		completeAssignment.clickNextButton();
+		testUtil.testWaitEight();
+		testUtil.scrollDown();
+		testUtil.testWaitFour();
+		completeAssignment.clickNextButton();
+		testUtil.testWaitEight();
+		testUtil.scrollDown();
+		testUtil.testWaitFour();
+		completeAssignment.clickVisibleDateTo();
+		testUtil.testWaitFour();
+		completeAssignment.selectVisibleDateTo();
+		testUtil.testWaitFour();
 		//completeAssignment.clickSaveButton();
-		
+		homePage.clickSignOutLink();
+		testUtil.testWaitFour();
+		homePage.clickSignOutBtn();
+		testUtil.testWaitFour();
+			
 	}
 	
 	@AfterMethod
 	public void tearDown() {
 		Driver.quit();
+		
 	}
 
 }
