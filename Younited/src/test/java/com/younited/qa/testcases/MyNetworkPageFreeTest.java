4f2ace5ed6da43d714ce2e87b25d91d11f2865cd
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-07-24 17:11:06 +0530

    MyNetworkPageFreeTest

--- Younited/src/test/java/com/younited/qa/testcases/MyNetworkPageFreeTest.java
+++ Younited/src/test/java/com/younited/qa/testcases/MyNetworkPageFreeTest.java
@@ -32,31 +32,95 @@ public class MyNetworkPageFreeTest extends TestBase{
 		myNetworkPageFree=new MyNetworkPageFree();
 		homePageFree=loginPage.loginHomePageFree(prop.getProperty("usernameone"), prop.getProperty("passwordone"));
 		testUtil.testWaitFourteen();
-		homePageFree.clickAcceptAllCookies();
-		testUtil.testWaitEight();
+		//try {
+			//homePageFree.clickAcceptAllCookies();
+		//}catch(NoSuchElementException e) {
+				//e.printStackTrace();
+		//}
+		//testUtil.testWaitFour();
+		//try {
+		    //homePageFree.suitableAssignments();
+		//}catch(NoSuchElementException e) {
+			//e.printStackTrace();
+		//}
+		//testUtil.testWaitFour();
 		homePageFree.clickNetworkTab();
 		testUtil.testWaitFour();
 		homePageFree.clickNetworkTabTooltip();
-		testUtil.testWaitFour();
-		communityPageFree.clickMyNetworkBtn();
 		testUtil.testWaitEight();
-		
+	
 	}
 	
 	@Test
 	public void myNetworkPageFreeTest()throws Exception {
+		myNetworkPageFree.clickSeachValue();
+		testUtil.testWaitTwo();
+		myNetworkPageFree.enterSearchValue("Test");
+		testUtil.testWaitFour();
+		myNetworkPageFree.clickCancelSearch();
+		testUtil.testWaitFour();
+		//Type of Message
+		myNetworkPageFree.clickTypeMessageDropDown();
+		testUtil.testWaitTwo();
+		myNetworkPageFree.selectTypeMessage();
+		testUtil.testWaitFour();
+		myNetworkPageFree.selectTypeMessage();
+		testUtil.testWaitFour();
+		myNetworkPageFree.clickTypeMessageDropDown();
+		testUtil.testWaitTwo();
+		//Area of Interest
+		myNetworkPageFree.clickAreaOfInterestDropDown();
+		testUtil.testWaitTwo();
+		myNetworkPageFree.selectAreaOfInterest();
+		testUtil.testWaitFour();
+		myNetworkPageFree.selectAreaOfInterest();
+		testUtil.testWaitFour();
+		myNetworkPageFree.clickAreaOfInterestDropDown();
+		testUtil.testWaitTwo();
+		//Sector
+		myNetworkPageFree.clickSectorDropDown();
+		testUtil.testWaitTwo();
+		myNetworkPageFree.selectSector();
+		testUtil.testWaitFour();
+		myNetworkPageFree.selectSector();
+		testUtil.testWaitFour();
+		myNetworkPageFree.clickSectorDropDown();
+		testUtil.testWaitTwo();
+		myNetworkPageFree.clickEveryOneDropDown();
+		testUtil.testWaitTwo();
+		myNetworkPageFree.selectEveryOneMyOwnNetwork();
+		testUtil.testWaitFour();
+		myNetworkPageFree.selectEveryOneEveryOne();
+		testUtil.testWaitFour();
+		myNetworkPageFree.clickEveryOneDropDown();
+		testUtil.testWaitTwo();
+		myNetworkPageFree.clickFavoiritesPosts();
+		testUtil.testWaitFour();
+		myNetworkPageFree.clickFavoiritesPosts();
+		testUtil.testWaitFour();
+		//MyNetwork
+		myNetworkPageFree.clickMyNetworkBtn();
+		testUtil.testWaitEight();
 		myNetworkPageFree.clickMyNetworkTab();
-		testUtil.testWaitEight();
+		testUtil.testWaitFour();
 		myNetworkPageFree.clickMyRequestsTab();
-		testUtil.testWaitEight();
+		testUtil.testWaitFour();
 		testUtil.scrollDown();
 		testUtil.testWaitFour();
+		testUtil.scrollUp();
+		testUtil.testWaitTwo();
 		myNetworkPageFree.clickSuggestionsForYouTab();
-		testUtil.testWaitEight();
+		testUtil.testWaitFour();
 		testUtil.scrollDown();
 		testUtil.testWaitFour();
+		testUtil.scrollUp();
+		testUtil.testWaitTwo();
 		myNetworkPageFree.clickAreaOfInterest();
-		testUtil.testWaitEight();
+		testUtil.testWaitFour();
+		homePageFree.clickSignOutLink();
+		testUtil.testWaitFour();
+		homePageFree.clickSignOutBtn();
+		testUtil.testWaitFour();
 	}
 	
 	@AfterMethod
