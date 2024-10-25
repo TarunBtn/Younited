e9d8a6e2d23972d3ad0416f80724c259fa61af67
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-10-25 17:53:47 +0530

    TestBaseBrowserStack

--- Younited/src/main/java/com/younited/qa/base/TestBaseBrowserStack.java
+++ Younited/src/main/java/com/younited/qa/base/TestBaseBrowserStack.java
@@ -1,88 +1,69 @@
 package com.younited.qa.base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.lang.reflect.Method;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
-
+import com.younited.qa.util.TestUtil;
 
 public class TestBaseBrowserStack {
-	
+
 	public static WebDriver Driver;
 	public static Properties prop;
-	public static final String USERNAME="tarunbutani_PiOvc9";
-	public static final String AUTOMATE_KEY="jpizFmPN3srPvMn6uf39";
-	public static final String URL="https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
-	
-	@Parameters({"os", "osVersion", "browserName", "browserVersion"})
-	@BeforeMethod
-	public void setUp(String os, String osVersion, String browserName, String browserVersion, Method name) {
-		
-		System.out.println("Browser name is: "+browserName);
-		String methodName=name.getName();
-		
-		DesiredCapabilities caps=new DesiredCapabilities();
-		
-		caps.setCapability("os", os);
-		caps.setCapability("osVersion", osVersion);
-		caps.setCapability("browserName", "Chrome");
-		caps.setCapability("browserVersion", browserVersion);
-		caps.setCapability("name", methodName);
-		
-		if(browserName.equals("chrome")) {
-			//WebDriverManager.chromedriver().setup();
-			caps.setCapability("browser", "Chrome");
-		}else if(browserName.equals("firefox")) {
-			//WebDriverManager.firefoxdriver().setup();
-			caps.setCapability("browser", "Firefox");
-		}
-		
-		try {
-			Driver=new RemoteWebDriver(new URL(URL), caps);
-			Driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);		
-		}catch(MalformedURLException e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	public TestBaseBrowserStack() {
-		try {
-			prop=new Properties();
-			FileInputStream ip=new FileInputStream("C:\\Users\\no02\\git\\Younited\\Younited\\src\\main\\java\\com\\younited\\qa\\config\\config.properties");
-			prop.load(ip);
-		}catch(FileNotFoundException e) {
-			e.printStackTrace();
-		}catch(IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void initialization()throws Exception {
-		Driver.manage().window().maximize();
-		Driver.manage().deleteAllCookies();
-		Driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-		Driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		
-		Driver.get(prop.getProperty("url"));
-		Thread.sleep(2000);
-	}
 
-	
-	@AfterMethod(alwaysRun=true)
-	public void tearDown() {
-		Driver.quit();
-	}
+    public static final String USERNAME = "your_username";
+    public static final String AUTOMATE_KEY = "your_access_key";
+    public static final String BROWSERSTACK_URL = "https://" + "tarunbutani_PiOvc9" + ":" + "jpizFmPN3srPvMn6uf39" + "@hub-cloud.browserstack.com/wd/hub";
+
+    public TestBaseBrowserStack() {
+      try {
+         prop = new Properties();
+         FileInputStream ip = new FileInputStream("C:\\Users\\tbutani\\git\\YounitedOne\\YounitedOne\\src\\main\\java\\com\\younited\\qa\\config\\config.properties");
+         prop.load(ip);
+          } catch(FileNotFoundException e) {
+                   e.printStackTrace();
+          } catch(IOException e) {
+          e.printStackTrace();
+          }
+      }
+
+    public void initialization() throws Exception {
+        String browserName = prop.getProperty("browser");
+        if (browserName.equals("chrome")) {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+        Driver = new ChromeDriver();
+        } else if (browserName.equals("FF")) {
+          System.setProperty("webdriver.gecko.driver", "C:\\geckodriver\\geckodriver.exe");
+        Driver = new FirefoxDriver();
+        } else if (browserName.equals("browserstack")) {
+        	
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability("browser", "Chrome");
+            caps.setCapability("browser_version", "latest");
+            caps.setCapability("os", "Windows");
+            caps.setCapability("os_version", "10");
+            caps.setCapability("name", "Sample Test");
+
+    Driver = new RemoteWebDriver(new URL(BROWSERSTACK_URL), caps);
+}
+
+    Driver.manage().window().maximize();
+    Driver.manage().deleteAllCookies();
+    Driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+    Driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+    Driver.get(prop.getProperty("url"));
 
 }
+}
+
+
+
